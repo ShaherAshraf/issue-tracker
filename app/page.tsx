737d91f9333e300083856fbef8a67d08acb84a00
ShaherAshraf <shaherashraf77@gmail.com>
2024-08-18 03:30:46 +0300

    Try getServerSideProps method

--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,27 @@ import IssueChart from './IssueChart';
 import { Flex, Grid } from '@radix-ui/themes';
 import { Metadata } from 'next';
 
-export default async function Home() {
+export async function getServerSideProps() {
   const open = await prisma.issue.count({ where: { status: 'OPEN' } });
   const inProgress = await prisma.issue.count({ where: { status: 'IN_PROGRESS' } });
   const closed = await prisma.issue.count({ where: { status: 'CLOSED' } });
 
+  return {
+    props: {
+      open,
+      inProgress,
+      closed,
+    },
+  };
+}
+
+interface Props {
+  open: number;
+  inProgress: number;
+  closed: number;
+}
+
+export default async function Home({ open, inProgress, closed }: Props) {
   return (
     <Grid columns={{ initial: '1', md: '2' }} gap='5'>
       <Flex direction='column' gap='5'>
